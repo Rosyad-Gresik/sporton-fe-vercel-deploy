1be15bf4ee5bdee8ae82506363732e17aa843570
Muhammad Rosyad <muhammadrosyad425@gmail.com>
2026-01-17 21:11:26 +0700

    Update page.tsx

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -7,7 +7,7 @@ const ProductDetail = () => {
     <main className="container mx-auto py-40 flex gap-12">
       <div className="bg-primary-light aspect-square min-w-140 flex justify-center items-center">
         <Image
-          src="/images/products/product-4.png"
+          src="/images/products/product-9.png"
           width={550}
           height={550}
           alt="product 4 image"
