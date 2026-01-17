3451f395dbfab7d5e2217c038d6b21345c079686
Muhammad Rosyad <muhammadrosyad425@gmail.com>
2026-01-17 21:18:18 +0700

    Update page.tsx

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -7,7 +7,7 @@ const ProductDetail = () => {
     <main className="container mx-auto py-40 flex gap-12">
       <div className="bg-primary-light aspect-square min-w-140 flex justify-center items-center">
         <Image
-          src="/images/products/product-9.png"
+          src="/images/products/product-4.png"
           width={550}
           height={550}
           alt="product 4 image"
@@ -17,7 +17,7 @@ const ProductDetail = () => {
       <div className="w-full py-7">
         <h1 className="font-bold text-5xl mb-6">SportsOn HyperSoccer v2</h1>
         <div className="bg-primary-light rounded-full text-primary py-2 px-6 w-fit mb-5">
-          Football
+            Running
         </div>
         <p className="leading-loose mb-8">
           The SportsOn HyperSoccer v2 is engineered for the player who demands
@@ -28,7 +28,7 @@ const ProductDetail = () => {
           traction.
         </p>
         <div className="text-primary text-[32px] font-semibold mb-12">
-          {priceFormatter(450000)}
+          {priceFormatter(440000)}
         </div>
         <ProductActions />
       </div>
